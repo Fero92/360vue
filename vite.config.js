4b7088953dfefc01300836778245df046063a3b4
Fero92 <frantiseksiles@gmail.com>
2025-03-27 16:49:33 +0100

    oprava base path formatu

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 
 export default defineConfig({
-  base: '360vue/',
+  base: '/360vue/',
 
   plugins: [vue(),
     tailwindcss(),
